bc1a6af176a5f5f6de9e641b0726547ea5133320
who-ratuu <2410631170044@student.unsika.ac.id>
2026-09-02 11:14:24 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Pertemuan 2
+Route::get('/about', function () {
+    return 'Halaman About';
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'User: ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = 'Tamu') {
+    return $name;
+});
+
+Route::get('/user-id/{id}', function ($id) {
+    return 'User ID: ' . $id;
+})->where('id', '[0-9]+');
+
+use App\Models\User;
+
+Route::get('/user/{user}', function (User $user) {
+    return $user->name;
+});
+
+//Named Routes
+Route::get('/profile', function () {
+    return 'Halaman Profile';
+})->name('profile');
+
+//Routes Group
+Route::prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return 'Halaman Dashboard Admin';
+    });
+
+    Route::get('/users', function () {
+        return 'Halaman Users Admin';
+    });
+
+});
